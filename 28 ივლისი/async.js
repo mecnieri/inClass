7ec45c58ea166a2fa88d925732e3787e036550e9
mecnieri <abzogio@yahoo.com>
2018-07-28 16:22:57 +0400

    9

--- 28 ივლისი/async.js
+++ 28 ივლისი/async.js
@@ -1,4 +1,25 @@
 fetch('https://jsonplaceholder.typicode.com/posts/1')
-  .then(response => response.json())
-  .then(json => console.log(json))
-  .catch(err => console.log(err);)
+    .then(response => response.json())
+    .then(json => console.log(json))
+    .catch(err => console.log(err))
+    
+async function getJson() {
+    try {
+        let response = await fetch('https://jsonplaceholder.typicode.com/posts/1')
+        let json = await response.json()
+        console.log(json);
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+getJson();
+
+
+
+
+
+
+
+
+
