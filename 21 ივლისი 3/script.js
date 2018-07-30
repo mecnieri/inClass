6461fee826a278c8772b658f426de754cbe0ec7f
mecnieri <abzogio@yahoo.com>
2018-07-31 02:36:30 +0400

    789

--- 21 ივლისი 3/script.js
+++ 21 ივლისი 3/script.js
@@ -6,24 +6,45 @@
 
 
 
-let color;
 
-console.log(color);
-document.getElementById("div1").addEventListener("click", function myFunction() {
-    color = "red";
-    console.log(color);
+div1.addEventListener("click", function myFunction() {
+    color1 = window.getComputedStyle(div1, null).getPropertyValue("background-color");;
 })
-document.getElementById("div2").addEventListener("click", function myFunction() {
-    color = "yellow";
-    console.log(color);
+div2.addEventListener("click", function myFunction() {
+    color1 = window.getComputedStyle(div2, null).getPropertyValue("background-color");;
 })
-document.getElementById("div3").addEventListener("click", function myFunction() {
-    color = "green";
-    console.log(color);
+div3.addEventListener("click", function myFunction() {
+    color1 = window.getComputedStyle(div3, null).getPropertyValue("background-color");
 })
 
-document.getElementById("createColor").addEventListener("click", function myFunction() {
-    var x = document.getElementById("div4");
-    x.setAttribute("style", "background-color:" + color);
+createColor.addEventListener("click", function myFunction() {
+    div10.setAttribute("style", "background-color:" + color1);
+})
 
+div4.addEventListener("click", function myFunction() {
+    color2 = window.getComputedStyle(div4, null).getPropertyValue("background-color");
+})
+div5.addEventListener("click", function myFunction() {
+    color2 = window.getComputedStyle(div5, null).getPropertyValue("background-color");
+})
+div6.addEventListener("click", function myFunction() {
+    color2 = window.getComputedStyle(div6, null).getPropertyValue("background-color");;
+})
+
+createColor.addEventListener("click", function myFunction() {
+    div11.setAttribute("style", "background-color:" + color2);
+})
+
+div7.addEventListener("click", function myFunction() {
+    color3 = window.getComputedStyle(div7, null).getPropertyValue("background-color");;
+})
+div8.addEventListener("click", function myFunction() {
+    color3 = window.getComputedStyle(div8, null).getPropertyValue("background-color");;
+})
+div9.addEventListener("click", function myFunction() {
+    color3 = window.getComputedStyle(div9, null).getPropertyValue("background-color");;
+})
+
+createColor.addEventListener("click", function myFunction() {
+    div12.setAttribute("style", "background-color:" + color3);
 })
