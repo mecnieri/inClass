5b71adce8f462e9c386e4ca090f617e58f857856
mecnieri <abzogio@yahoo.com>
2018-08-04 10:13:00 +0400

    123

---
+++ car constructor/script.js
@@ -0,0 +1,12 @@
+function Car(setName, setColor) {
+    this.setName() {
+        this.setName = setName;
+        return this;
+    }
+    ,
+    this.setColor(){
+        this.setColor = setColor;
+        return this;
+    }
+
+}
