4ec9e84b3530b57f668a45ccfab67291d2c268cf
mecnieri <abzogio@yahoo.com>
2018-08-07 06:01:10 +0400

    12312313

---
+++ udemy/DOM challenge/script.js
@@ -0,0 +1,13 @@
+let textH1 = document.createTextNode('es aris h1')
+let h1 = document.createElement('h1')
+h1.appendChild(textH1)
+
+let misamarti = document.body
+console.log(misamarti);
+let misamarti2 = document.getElementById("c")
+console.log(misamarti2);
+
+ 
+misamarti2.appendChild(h1)
+misamarti2.appendChild(h1)
+misamarti2.appendChild(h1)
