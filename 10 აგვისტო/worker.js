0b96565e958aec34337ceea97dbece79016a4203
mecnieri <abzogio@yahoo.com>
2018-08-10 21:14:55 +0400

    456789

--- 10 აგვისტო/worker.js
+++ 10 აგვისტო/worker.js
@@ -5,3 +5,15 @@ function add(a, b) {
 function sub(a, b) {
     this.postMessage(a - b)
 }
+
+
+self.addEventListener('message', function (e) {
+    // console.log(e.data);
+    switch (e.data.type) {
+        case 'add':
+        add.apply(this, e.data.nums)
+        break;
+        case 'sub':
+        sub.apply(this, e.data.nums)
+        break;
+}});
