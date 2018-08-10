0568fb89eb225277c5c475f01a945cec6e90a290
mecnieri <abzogio@yahoo.com>
2018-08-10 17:11:04 +0400

    ასდქწ

--- udemy/063/script.js
+++ udemy/063/script.js
@@ -3,15 +3,13 @@ $(document).ready(function () {
     let h = document.getElementById('search-text')
     let notesID = document.getElementById('notes')
 
-
-    $('#btn').click(function () {
+     $('#btn').click(function () {
         notes.push(h.value);
         localStorage.setItem("note title", notes)
         notesID.innerHTML = ""
         for (let i = 0; i < notes.length; i++) {
-
             notesID.innerHTML += `${notes[i]} <br>`
-        }
+         }
     })
-    console.log(JSON.parse(localStorage.getItem("note title")));
+     console.log(JSON.parse(localStorage.getItem("note title")));
 })
