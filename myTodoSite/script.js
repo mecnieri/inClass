8132ac55ac32e0d0894a85a296deea174b411d40
mecnieri <abzogio@yahoo.com>
2018-08-13 04:00:20 +0400

    123

--- myTodoSite/script.js
+++ myTodoSite/script.js
@@ -1,9 +1,9 @@
 let storage = window.localStorage;
 console.log(sololearnCount);
-let count = storage.getItem("sololearn");
 sololearnCount.innerHTML = count;
 sololearnCount.addEventListener("click", function () {
     count++;
     sololearnCount.innerHTML = count;
     storage.setItem("sololearn", count)
+    let count = storage.getItem("sololearn");
 })
