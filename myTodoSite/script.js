292f4ee93db1fb4fb12f463fdbebf84b60015da3
mecnieri <abzogio@yahoo.com>
2018-08-14 06:35:56 +0400

    wead

--- myTodoSite/script.js
+++ myTodoSite/script.js
@@ -1,7 +1,7 @@
 let storage = window.localStorage;
 let now = new Date()
 
-// sololearn
+//#region sololearn
 
 if (storage.sololearn == "undefined") {
     let countSolo = 0;
@@ -18,7 +18,28 @@ dt1.addEventListener("click", function () {
     countSolo = storage.getItem("sololearn");
     console.log(countSolo);
 })
+//#endregion 
 
+//#region 
+
+if (storage.freevids == "undefined") {
+    let countFreeVids = 0;
+    storage.setItem("sololearn", countSolo)
+    dt3.innerHTML = countFreeVids;
+}
+
+let countFreeVids = storage.getItem("freevids");
+dt3.innerHTML = countFreeVids;
+
+dt3.addEventListener("click", function () {
+    countFreeVids++;
+    dt3.innerHTML = countFreeVids;
+    storage.setItem("freevids", countFreeVids)
+    countFreeVids = storage.getItem("freevids");
+    console.log(countFreeVids);
+})
+
+//#endregion 
 
 for (let i = 1; i <= 6; i++) {
     let tomorrow = new Date(2018, 8, 18, 5)
@@ -28,20 +49,20 @@ for (let i = 1; i <= 6; i++) {
 }
 
 let freecodeVids = {
-    doneTasks: 3,
+    doneTasks: countFreeVids,
     allTasks: 100,
     start: new Date(2018, 7, 14),
     end: new Date(2018, 8, 17),
     duration() {
         return Math.round(this.end - this.start) / 1000 / 60 / 60 / 24;
     },
-    left(){
+    left() {
         return Math.round((this.end - now) / 1000 / 60 / 60 / 24);
     }
 };
- 
+
 (function inserts() {
-    pr3.innerHTML = (freecodeVids.doneTasks/freecodeVids.allTasks)*100 + "%"
+    pr3.innerHTML = (freecodeVids.doneTasks / freecodeVids.allTasks) * 100 + "%"
     dt3.innerHTML = freecodeVids.doneTasks;
     allTasks3.innerHTML = freecodeVids.allTasks;
 
@@ -49,5 +70,5 @@ let freecodeVids = {
     dl3.innerHTML = freecodeVids.end.getDate() + " - " + (freecodeVids.end.getMonth() + 1)
     dr3.innerHTML = freecodeVids.duration();
     dayLft3.innerHTML = freecodeVids.left();
-    
+
 })()
