d81dda1908404523002e9a57fe46767edcf9be48
mecnieri <abzogio@yahoo.com>
2018-08-16 18:19:33 +0400

    159

--- 16 აგვისტო/observerpattern.js
+++ 16 აგვისტო/observerpattern.js
@@ -5,7 +5,7 @@ let Task = require('./script');
 let notificationService = function () {
     let message = 'Notifying';
     this.update = function (task) {
-        console.log(`${message} ${task.user} for task ${task.name}`);
+        document.write(`${message} ${task.user} for task ${task.name}`);
     }
 }
 
