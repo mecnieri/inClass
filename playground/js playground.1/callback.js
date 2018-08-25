b92cbec20c68ffcb1b7c998a1f3848ab1d0b9771
mecnieri <abzogio@yahoo.com>
2018-08-25 15:16:51 +0400

    123

--- playground/js playground.1/callback.js
+++ playground/js playground.1/callback.js
@@ -23,3 +23,15 @@ say("I am from USA", helloMary)
 
 
 say(undefined, helloMary)
+
+
+// What is a Callback?
+// Simply put: A callback is a function that is to be executed
+// after another function has finished executing — hence the name ‘call back’.
+
+// More complexly put: 
+// In JavaScript, functions are objects.
+// Because of this, functions can take functions as arguments, 
+// and can be returned by other functions.
+// Functions that do this are called higher-order functions.
+// Any function that is passed as an argument is called a callback function.
---
+++ udemy/array/todo.js
@@ -0,0 +1,7 @@
+let todos = ["udemy", "codeacademy", "freecodecamp", "codewars", "the net ninja"]
+
+console.log(`I have ${todos.length} to do `);
+
+
+
+console.log(todos);
