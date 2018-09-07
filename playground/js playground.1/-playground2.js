8f60e8ff873e42c70616bba4c6e87a00df7130c4
mecnieri <abzogio@yahoo.com>
2018-09-08 01:10:51 +0400

    123

--- playground/js playground.1/-playground2.js
+++ playground/js playground.1/-playground2.js
@@ -18,25 +18,74 @@
 
 // Exercise 14 
 
-let john = { name: "John", age: 25 };
-let adam = { name: "Adam", age: 30 };
-let mary = { name: "Mary", age: 28 };
-let arr = [john, adam, mary];
+// let john = { name: "John", age: 25 };
+// let adam = { name: "Adam", age: 30 };
+// let mary = { name: "Mary", age: 28 };
+// let arr = [john, adam, mary];
 
-// // for(let name of arr){
-// //     console.log(name.name);
-// // }
+// // // for(let name of arr){
+// // //     console.log(name.name);
+// // // }
 
 
-function sortByName() {
-    //    return users.map(item => item.name).sort()
-    arr.sort((a, b) => a.name > b.name)
-}
-// sortByName(arr);//
-//  now: [adam, john, mary]
-//  alert(arr[1].name); // John
+// function sortByName() {
+//     //    return users.map(item => item.name).sort()
+//     arr.sort((a, b) => a.name > b.name)
+// }
+// // sortByName(arr);//
+// //  now: [adam, john, mary]
+// //  alert(arr[1].name); // John
 
-sortByName()
-console.log(arr);
-console.log(arr[1].name);
+// sortByName()
+// console.log(arr);
+// console.log(arr[1].name);
 
+// function steps(st) {
+//     let result = ["'"]
+//     for (let o = 0; o < 2*st; o++) {
+//         result.push(' ')
+//     }
+
+//     for (let i = 0; i < st; i++) {
+//         result.length = 2*st;
+//         result[result.length] = "'"
+//         result[st-i] = 'w'
+//         result[st] = 'w'
+//         result[st+i] = 'w'
+        
+//         let a = result.join('')
+//         console.log(a);
+//      }
+// }
+// steps(10);
+
+
+// function printNum(n) {
+//     if (n === 0) {
+//         return;
+//     }
+
+//     console.log(n);
+//     printNum(n - 1)
+// }
+
+// printNum(10)
+
+
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+// let chuncked = []
+
+// function chunck(array, length) {
+
+//     let len = Math.ceil(arr.length / length);
+//     for (let i = 0; i < len; i++) {
+//         chuncked.push(new Array)
+
+//     }
+//     array.forEach(element => {
+//         chuncked[Math.floor(arr.indexOf(element) / length)].push(element)
+//     });
+//     return chuncked
+// }
+
+// console.log(chunck(arr, 2));
--- udemy/array/todo.js
+++ udemy/array/todo.js
@@ -101,3 +101,22 @@ check.addEventListener('change', function (e) {
 // console.log(account.getAccountSummary());
 
 // console.log(console)
+
+for (let i = 0; i <= 100; i++) {
+    debugger;
+
+    if (i % 3 == 0 && i % 5 !== 0) {
+        console.log("fizz");
+    }
+    else if (i % 5 == 0 && i % 3 !== 0) {
+        console.log('bizz');
+    }
+    else if (
+        i % 5 == 0 && i % 3 == 0
+    ) {
+        console.log('fizzbizz');
+    }
+    else {
+        console.log(i);
+    }
+}
