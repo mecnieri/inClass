a22bb821dd492b0eefc4a0498ff690c2099c2ca4
mecnieri <abzogio@yahoo.com>
2018-09-12 03:22:15 +0400

    asd

--- myTodoSite/script.js
+++ myTodoSite/script.js
@@ -1,6 +1,6 @@
 let storage = window.localStorage;
-let now = new Date()
-let month = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+let now = new Date();
+let month = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
 class Todo {
     constructor(id, name, doneTasks, allTasks, start, end) {
         this.id = id;
@@ -41,20 +41,17 @@ let freecodeVids = new Todo(4, 'freecodeVids', 0, 100, new Date(2018, 7, 14), ne
 let algorithms = new Todo(7, 'algorithms', 43, 133, new Date(2018, 8, 11), new Date(2018, 8, 24));
 let udemy = new Todo(6, 'udemy', 63, 155, new Date(2018, 6, 11), new Date(2018, 8, 24));
 let fundamentals = new Todo(8, 'fundamentals', 13, 85, new Date(2018, 8, 11), new Date(2018, 8, 24));
-let sound = new Audio()
-sound.src = "Button Positive.mp3"
-let arr = [freecodeVids, algorithms, fundamentals, udemy]
-if (storage.freecodeVids == "undefined") {
-    freecodeVids.doneTasks = 0;
-} else {
+let arr = [freecodeVids, algorithms, fundamentals, udemy];
+let sound = new Audio();
+sound.src = "Button Positive.mp3";
+if (storage.freecodeVids !== "undefined") {
     freecodeVids.doneTasks = storage.getItem("freecodeVids");
     algorithms.doneTasks = storage.getItem("algorithms");
     fundamentals.doneTasks = storage.getItem("fundamentals");
     udemy.doneTasks = storage.getItem("udemy");
-
 }
 
-function inserts(obj) {
+function updateTable(obj) {
     document.getElementById('dt' + obj.id).innerHTML = obj.doneTasks;
     document.getElementById('pr' + obj.id).innerHTML = obj.progress();
     document.getElementById('advnc' + obj.id).innerHTML = obj.advanced();
@@ -66,11 +63,7 @@ function inserts(obj) {
     document.getElementById('dayLft' + obj.id).innerHTML = obj.left();
 
 }
-inserts(freecodeVids);
-inserts(algorithms);
-inserts(fundamentals);
-inserts(udemy);
-
+for (o of arr) { updateTable(o) }
 document.querySelectorAll('.doneTasks')[0].addEventListener('click', function (e) {
     let idd = Number(e.target.id.slice(2));
     let ob = arr.find(t => t.id === idd);
@@ -78,23 +71,32 @@ document.querySelectorAll('.doneTasks')[0].addEventListener('click', function (e
     storage.setItem(`${ob.name}`, ob.doneTasks)
     ob.doneTasks = storage.getItem(`${ob.name}`);
     sound.play();
-    inserts(ob)
+    updateTable(ob)
 });
 
-function checkFunc() {
-    var checkBox = document.getElementById("myCheck");
-    if (checkBox.checked == true) {
-        document.querySelectorAll('.startingTime')[0].style.display = "none";
-        document.querySelectorAll('.duration')[0].style.display = "none";
-        document.querySelectorAll('.deadLine')[0].style.display = "none";
-        document.querySelectorAll('.allTasks')[0].style.display = "none";
-        document.querySelectorAll('.timeProgress')[0].style.display = "none";
+// #region show only main 
+
+if (myCheck.checked) {
+    function checkFunc() {
+        if (myCheck.checked == true) {
+            document.querySelectorAll('.startingTime')[0].style.display = "none";
+            document.querySelectorAll('.duration')[0].style.display = "none";
+            document.querySelectorAll('.deadLine')[0].style.display = "none";
+            document.querySelectorAll('.allTasks')[0].style.display = "none";
+            document.querySelectorAll('.timeProgress')[0].style.display = "none";
+        }
+        else {
+            document.querySelectorAll('.startingTime')[0].style.display = "block";
+            document.querySelectorAll('.duration')[0].style.display = "block";
+            document.querySelectorAll('.deadLine')[0].style.display = "block";
+            document.querySelectorAll('.allTasks')[0].style.display = "block";
+            document.querySelectorAll('.timeProgress')[0].style.display = "block";
+        }
     }
-    else {
-        document.querySelectorAll('.startingTime')[0].style.display = "block";
-        document.querySelectorAll('.duration')[0].style.display = "block";
-        document.querySelectorAll('.deadLine')[0].style.display = "block";
-        document.querySelectorAll('.allTasks')[0].style.display = "block";
-        document.querySelectorAll('.timeProgress')[0].style.display = "block";
-    }
+    checkFunc()
 }
+
+//#endregion 
+
+
+pr3.style.background = 'rgb(' + 200 + ',' + 100 + ',' + 192 + ')'
