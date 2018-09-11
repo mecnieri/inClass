e03173c4d4ef9855fe084b78e2ad01771bc5fa45
mecnieri <abzogio@yahoo.com>
2018-09-11 21:53:49 +0400

    123

--- myTodoSite/script.js
+++ myTodoSite/script.js
@@ -2,13 +2,15 @@ let storage = window.localStorage;
 let now = new Date()
 let month = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
 class Todo {
-    constructor(id, doneTasks, allTasks, start, end) {
+    constructor(id, name, doneTasks, allTasks, start, end) {
         this.id = id;
+        this.name = name;
         this.doneTasks = doneTasks;
         this.allTasks = allTasks;
         this.start = start;
         this.allTasks = allTasks;
         this.end = end;
+        this.timeProgress = (this.duration() - this.left()) / this.duration() * 100 + "%"
     }
     get duration() {
         return this.calcDuration;
@@ -23,20 +25,20 @@ class Todo {
         return Math.round((this.end - now) / 1000 / 60 / 60 / 24);
     }
 }
-let freecodeVids = new Todo(3, 0, 100, new Date(2018, 7, 14), new Date(2018, 8, 17));
-let algorithms = new Todo(7, 43, 133, new Date(2018, 8, 11), new Date(2018, 8, 24));
-let udemy = new Todo(6, 63, 155, new Date(2018, 6, 11), new Date(2018, 8, 24));
-let fundamentals = new Todo(8, 13, 85, new Date(2018, 8, 11), new Date(2018, 8, 24));
+let freecodeVids = new Todo(3, 'freecodeVids', 0, 100, new Date(2018, 7, 14), new Date(2018, 8, 17));
+let algorithms = new Todo(7, 'algorithms', 43, 133, new Date(2018, 8, 11), new Date(2018, 8, 24));
+let udemy = new Todo(6, 'udemy', 63, 155, new Date(2018, 6, 11), new Date(2018, 8, 24));
+let fundamentals = new Todo(8, 'fundamentals', 13, 85, new Date(2018, 8, 11), new Date(2018, 8, 24));
 let sound = new Audio()
 sound.src = "Button Positive.mp3"
 let arr = [freecodeVids, algorithms, fundamentals, udemy]
 if (storage[3] == "undefined") {
     freecodeVids.doneTasks = 0;
 } else {
-    freecodeVids.doneTasks = storage.getItem("3");
-    algorithms.doneTasks = storage.getItem("7");
-    fundamentals.doneTasks = storage.getItem("8");
-    udemy.doneTasks = storage.getItem("6");
+    freecodeVids.doneTasks = storage.getItem("freecodeVids");
+    algorithms.doneTasks = storage.getItem("algorithms");
+    fundamentals.doneTasks = storage.getItem("fundamentals");
+    udemy.doneTasks = storage.getItem("udemy");
 
 }
 
@@ -60,8 +62,8 @@ document.querySelectorAll('.doneTasks')[0].addEventListener('click', function (e
     let idd = Number(e.target.id.slice(2));
     let ob = arr.find(t => t.id === idd);
     ob.doneTasks++;
-    storage.setItem(`${ob.id}`, ob.doneTasks)
-    ob.doneTasks = storage.getItem(`${ob.id}`);
+    storage.setItem(`${ob.name}`, ob.doneTasks)
+    ob.doneTasks = storage.getItem(`${ob.name}`);
     sound.play();
     inserts(ob)
 });
