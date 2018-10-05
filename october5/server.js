fb7d5a996da7a4376e4f70596e4e67c5267efc94
mecnieri <abzogio@yahoo.com>
2018-10-05 20:24:33 +0400

    asd

--- october5/server.js
+++ october5/server.js
@@ -1,14 +1,28 @@
 const express = require('express')
 const app = express();
-
+let id = 0
+let products = [{ name: "Apple", id: id++, price: 4000 },
+{ name: "Samsung", id: id++, price: 3500 }];
 
 app.get('/', (req, res) => {
-    res.send('Hello');
+    res.send('Hello dasd');
 })
 
 app.get('users/:userId', (req, res) => {
     const userId = req.params.userId;
     res.send(`user ID - ${userId}`)
 })
+
+// query string
+app.get('/products', (req, res) => {
+    res.send(
+        products.map(product => {
+            return `<h2 id="product-${product.id}" data-id="${product.id}" 
+            onclick="window.location.replace('http://localhost:3000/products/${product.id}')"> ${product.name} </h2>`
+        }).join('')
+    )
+})
+
+
 
 app.listen(3000)
