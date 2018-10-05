96f3c0c0bc405fa9b17aab8340c4bdd6a519dbc3
mecnieri <abzogio@yahoo.com>
2018-10-05 21:30:43 +0400

    789

--- october5/server.js
+++ october5/server.js
@@ -24,18 +24,44 @@ app.get('/products', (req, res) => {
 })
 
 
-app.get('/products/:productId', (req, res)=>{
+app.get('/products/:productId', (req, res) => {
     const id = req.params.productId;
-    const Product = products[id];
-    const response = `
+    const Product = products.find(prod => prod.id == id);
+
+    let response = "product not found"
+    if (Product) {
+        res.send(`
     <a href="http://localhost:3000/products/"> Go back </a>
     <h1>${Product.name} </h1>
     <hr>
     <h2>Price $${Product.price}, Id - ${Product.id} </h2>
-    `;
+    `)
+    }
 
     res.send(response)
 })
+
+//CRUD 
+//CREATE/READ/UPDATE/DELETE 
+
+app.post('/products/:id/:name/:price', (req, res) => {
+    let product = {
+        id: req.params.id,
+        name: req.params.name,
+        price: req.params.price,
+    }
+    products.push(product)
+    res.json({ message: "product created", product })
+});
+
+app.delete('/products/:id', (req, res) => {
+    const id = products.findIndex(p => p.id == req.params.id)
+    const product = products[id]
+    products.splice(id, 1)
+    res.json({ message: "product deleted", product })
+
+})
+
 
 
 app.listen(3000)
