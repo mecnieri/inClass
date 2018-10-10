6111f5b22579a45093e892dc851458bd359058d2
mecnieri <abzogio@yahoo.com>
2018-10-10 21:14:36 +0400

    123

--- 10octomber/src/routes/productRoutes.js
+++ 10octomber/src/routes/productRoutes.js
@@ -2,15 +2,22 @@ const express = require('express');
 const router = express.Router();
 
 let products = [
-    { price: 1000000000, title: "Apple" },
-    { price: 10000000, title: "Google" },
-    { price: 1000000, title: "Nokia" },
+    { price: 200000000, title: "Apple", id: 0 },
+    { price: 30000000000, title: "Google", id: 1 },
+    { price: 10000000000000, title: "Nokia", id: 2 },
 
 ]
 
 router.get('/', (req, res) => {
     res.render('products/index', { title: "Products Page", products })
 })
+
+router.get('/:id(\\d)', (req, res) => {
+    let id = req.params.id
+    res.send(id)
+    res.render('products/detail', { title: `product - ${id}`, products })
+})
+
 router.get('/add', (req, res) => {
     res.json({ message: "Products Add page" })
 })
