c6d733824a645f1f817254418d43262a18415e08
mecnieri <abzogio@yahoo.com>
2018-10-17 20:55:16 +0400

    upload

--- 17october/script.js
+++ 17october/script.js
@@ -3,27 +3,27 @@ const path = require('path')
 const multer = require('multer')
 const app = express()
 
-const cookiePerser = require('cookie-parser')
+const cookieParser = require('cookie-parser')
 const session = require('express-session')
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, '/public')));
 app.set('view engine', 'pug');
-app.set('views', './public/views')
+app.set('views', 'public/views')
 const multerConfig = multer({
     dest: 'public/uploads',
-
 })
 
-// app.use(cookiePerser())
+app.use(cookieParser())
 app.get('/', (req, res) => {
-
+    res.sendFile(path.join(__dirname, '/public', 'upload.html'))
 })
 
 // multi-part/form-data
 app.post('/', multerConfig.single('image'), (req, res) => {
-    const imgUrl = path.join('uploads', req.image.filename)
-    res.render('index', { imgUrl })
+    const username = req.body.username;
+    const imgUrl = path.join('uploads', req.file.filename)
+    res.render('index', { imgUrl, username })
 })
 
 app.listen(PORT, () => {
