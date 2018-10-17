0399aa43f6616c92c207847c5c5b715c76120034
mecnieri <abzogio@yahoo.com>
2018-10-17 09:35:58 +0400

    exercise

---
+++ Exercise/display-time/index.js
@@ -0,0 +1,8 @@
+let MM = new Date().getMinutes()
+let HH = new Date().getHours()
+let dd = new Date().getDate()
+let mm = new Date().getMonth()
+let yyyy = new Date().getFullYear()
+
+
+console.log(`${dd}/${mm}/${yyyy} ${HH}:${MM}`);
