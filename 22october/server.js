6b58c394db16ea6d1a4831850d83cf6752f99642
mecnieri <abzogio@yahoo.com>
2018-10-22 20:53:45 +0400

    qwe

---
+++ 22october/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require("path")
+const MoviesController = require("./routes/moviesControllers");
+const app = express();
+app.use(express.json())
+
+app.use(express.static(path.join(__dirname, "/public")));
+app.use('/api', MoviesController);
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public', 'index.html'))
+})
+
+app.listen(3000)
