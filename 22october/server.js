40c9bc47b74139c416d1a5a20281f8dea278eb56
mecnieri <abzogio@yahoo.com>
2018-10-24 13:38:49 +0400

    565555

--- 22october/server.js
+++ 22october/server.js
@@ -6,10 +6,8 @@ app.use(express.json())
 
 app.use(express.static(path.join(__dirname, "/public")));
 app.use('/api/v1', MoviesController);
-// app.use('/api/v1', MoviesController);
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/public', 'index.html'))
 })
-
 app.listen(3000)
