1ed2e0a9b1651035235cf19862b3e5608dc82cef
mecnieri <abzogio@yahoo.com>
2018-11-07 04:25:16 +0400

    playground2.0

--- playground/js playground.1/remove.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("removeDay").addEventListener("click", function () {
-
-     // Removes an element from the document
-    let element = document.querySelector(".cla").lastElementChild;
-    element.parentNode.removeChild(element);
-    a--;
-});
