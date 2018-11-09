e5afc0a8dd65b1193883a879978b124bc4dec1b7
mecnieri <abzogio@yahoo.com>
2018-11-09 21:22:02 +0400

    789

---
+++ 9november/myapp/server.js
@@ -0,0 +1,36 @@
+const fs = require('fs')
+const path = require('path')
+const express = require('express')
+const data = require('./data/data.json')
+const app = express()
+let id = 0;
+
+
+const PORT = 3000;
+// app.set('port', (process.env.PORT || 3000))
+
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+app.use((req, res, next) => {
+    res.setHeader('Cache-Control', 'no-cache');
+    next();
+})
+
+app.get('/api/todos', (req, res) => {
+    res.send(data)
+})
+
+app.get('*', (req, res) => {
+    fs.readFile(`${__dirname}/public/index.html`, (err, html) => {
+        if(err) throw err;
+        res.setHeader('Content-Type', 'text/html')
+        res.send(html)
+    })
+})
+
+
+app.listen(PORT)
+
+
