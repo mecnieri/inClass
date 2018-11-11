b75747a909ea4708ebc4a53e1e8a1242e6a73f98
mecnieri <abzogio@yahoo.com>
2018-11-11 06:53:12 +0400

    წრწ

--- React/8november/myapp/src/index.js
+++ React/8november/myapp/src/index.js
@@ -1,12 +1,38 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+let gamosacheni = (
+    <div>
+        <h1>ეს მინდა გამოჩნდეს</h1>
+        <p>ესეც პარაგრაფი</p>
+    </div>
+    )
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+let idishi = document.getElementById('root')
+
+
+ReactDOM.render(gamosacheni, idishi)
+
+
+
+
+
+
+
+
+
+
+
+
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App';
+// import * as serviceWorker from './serviceWorker';
+
+// ReactDOM.render(<App />, document.getElementById('root'));
+
+// // If you want your app to work offline and load faster, you can change
+// // unregister() to register() below. Note this comes with some pitfalls.
+// // Learn more about service workers: http://bit.ly/CRA-PWA
+// serviceWorker.unregister();
