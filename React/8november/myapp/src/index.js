d6837c9ffafd490841b108b9427345e2aa6ed4d8
mecnieri <abzogio@yahoo.com>
2018-11-14 07:28:33 +0400

    da

--- React/8november/myapp/src/index.js
+++ React/8november/myapp/src/index.js
@@ -1,45 +1,91 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
 
-const obj = {
-    title: "Visibility toggle",
-    text: "so, here is some toggle text",
-    disable: true
-}
-const handleDisable = (e) => {
-    e.preventDefault();
-    obj.disable === true ? obj.disable = false : obj.disable = true;
-    renderToggle();
-}
-
-const i = document.getElementById('root')
-
-const renderToggle = () => {
-    const app = (
-        <div>
-            <h1>{obj.title}</h1>
-            {!obj.disable && <p >{obj.text}</p>}
-            <button onClick={handleDisable}>toggle</button>
-        </div >
-    )
-
-    ReactDOM.render(app, i)
+class Incedision extends React.Component {
+    render() {
+        const title = 'Incedision';
+        const subtitle = 'Put your life in computers hands'
+        const options = ['one', 'two', 'three', 'four']
+        return (
+            <div>
+                <Header title={title} subtitle={subtitle} />
+                <Action />
+                <Options opts={options} />
+                <AddOption />
+            </div>
+        )
+    }
 }
 
-renderToggle();
+class Header extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>{this.props.title}</h1>
+                <p>{this.props.subtitle}</p>
+            </div>
+        )
+    }
+}
+class Action extends React.Component {
+    render() {
+        return (
+            <div>
+                <button>What should I do ? </button>
+            </div>
+        )
+    }
+}
+class Options extends React.Component {
+    handleRemove() {
+        alert("Remove all")
+    }
+    render() {
+        return (
+            <div>
+                <button onClick={this.handleRemove}>Remove All</button>
+                {
+                    this.props.opts.map((element, index) => <Option key={index} el={element} />)
+                }
+            </div>
+        )
+    }
+}
+class Option extends React.Component {
+    render() {
+        return (
+            <div>
+                {
+                    <p>{this.props.el}</p>
+                }
+            </div>
+        )
+    }
+}
+
+
+
+class AddOption extends React.Component {
+    handleAddTo(e) {
+        e.preventDefault()
+        debugger
+        if(e.target.children.inp.value){
+            alert(e.target.children.inp.value)
+        }
+    }
+    render() {
+        return (
+            <div>
+                <form onSubmit={this.handleAddTo}>
+                    <input name="inp" />
+                    <button>ADD</button>
+                </form>
+            </div>
+        )
+    }
+}
 
 
 
 
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import './index.css';
-// import App from './App';
-// import * as serviceWorker from './serviceWorker';
-
-// ReactDOM.render(<App />, document.getElementById('root'));
-
-// // If you want your app to work offline and load faster, you can change
-// // unregister() to register() below. Note this comes with some pitfalls.
-// // Learn more about service workers: http://bit.ly/CRA-PWA
-// serviceWorker.unregister();
+ReactDOM.render(<Incedision />, document.getElementById('root'))
